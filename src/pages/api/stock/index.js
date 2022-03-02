7d0d499b4f174e6857c561426d16a37886277980
XiaoXuxxxx <aonrok555@gmail.com>
2022-03-03 00:54:07 +0700

    feat: generate the price if not exist

--- src/pages/api/stock/index.js
+++ src/pages/api/stock/index.js
@@ -6,6 +6,7 @@ import moment from 'moment'
 
 const handler = nextConnect()
 
+const STOCK_SYMBOL_LIST = ['MINT', 'ECML', 'HCA', 'LING', 'MALP']
 const OPEN_MARKET_TIME = { hour: 9, minute: 0, second: 0 }
 const CLOSE_MARKET_TIME = { hour: 22, minute: 0, second: 0 }
 
@@ -24,21 +25,12 @@ handler.get(async (req, res) => {
   const closeTime = moment().set(CLOSE_MARKET_TIME)
   const subtract = +!(moment().isBetween(openTime, moment().endOf('day')))
 
-  const lastStocks = await StockHistory
-    .find({ date: moment().startOf('day').subtract(subtract, 'day').toDate() })
-    .select('-_id symbol rate')
-    .lean()
-    .exec()
+  const closedStockHistory = await getStockHistory(subtract + 1)
+  const latestStockHistory = await getStockHistory(subtract)
 
-  const closedStocks = await StockHistory
-    .find({ date: moment().startOf('day').subtract(1 + subtract, 'day').toDate() })
-    .select('-_id symbol rate')
-    .lean()
-    .exec()
-
-  const stocks = lastStocks.map(stock => (
-    {...stock, changed: stock.rate - closedStocks.find(e => e.symbol == stock.symbol).rate }
-  ))
+  const stocks = latestStockHistory.map(stock => {
+    return { ...(stock?._doc ?? stock), changed: stock.rate - closedStockHistory.find(e => e.symbol == stock.symbol).rate }
+  })
 
   res.status(200)
     .json({
@@ -50,5 +42,44 @@ handler.get(async (req, res) => {
       timestamp: new Date()
     })
 })
+
+const getStockHistory = async (subtraction) => {
+  let stockHistory = await StockHistory
+    .find({ date: moment().startOf('day').subtract(subtraction, 'day').toDate() })
+    .select('-_id symbol rate')
+    .lean()
+    .exec()
+
+  if (stockHistory.length < STOCK_SYMBOL_LIST.length) {
+    stockHistory = await includeMissingStockHistory(stockHistory, subtraction)
+  }
+
+  return stockHistory
+}
+
+const includeMissingStockHistory = async (stockHistory, subtraction) => {
+  const result = []
+  const existedStockSymbols = stockHistory.map(stock => stock.symbol)
+  const missingStockSymbols = STOCK_SYMBOL_LIST.filter(symbol => !existedStockSymbols.includes(symbol))
+
+  await Promise.all(missingStockSymbols.map(async (symbol) => {
+    const foundOldStockHistory = await StockHistory
+      .findOne({ symbol: symbol })
+      .sort({ date: -1 })
+      .select('-_id rate')
+      .lean()
+      .exec()
+
+    const newStockHistory = await StockHistory.create({
+      symbol,
+      rate: foundOldStockHistory?.rate || 1,
+      date: moment().startOf('day').subtract(subtraction, 'day').toDate(),
+    })
+
+    result.push(newStockHistory)
+  }))
+
+  return [...result, ...stockHistory]
+}
 
 export default handler
